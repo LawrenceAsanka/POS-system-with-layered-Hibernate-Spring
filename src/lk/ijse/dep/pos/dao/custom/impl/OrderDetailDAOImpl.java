805336fde2f64632f51301c64b6581ee3030342e
LawrenceAsanka <asankalorance@gmail.com>
2020-09-04 12:41:03 +0530

    Update dao/custom/impl

--- src/lk/ijse/dep/pos/dao/custom/impl/OrderDetailDAOImpl.java
+++ src/lk/ijse/dep/pos/dao/custom/impl/OrderDetailDAOImpl.java
@@ -4,7 +4,9 @@ import lk.ijse.dep.pos.dao.CrudDAOImpl;
 import lk.ijse.dep.pos.dao.custom.OrderDetailDAO;
 import lk.ijse.dep.pos.entity.OrderDetail;
 import lk.ijse.dep.pos.entity.OrderDetailPK;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderDetailDAOImpl extends CrudDAOImpl<OrderDetail,OrderDetailPK> implements OrderDetailDAO {
 
 
--- src/lk/ijse/dep/pos/dao/custom/impl/QueryDAOImpl.java
+++ src/lk/ijse/dep/pos/dao/custom/impl/QueryDAOImpl.java
@@ -5,9 +5,11 @@ import lk.ijse.dep.pos.entity.CustomEntity;
 
 import org.hibernate.Session;
 import org.hibernate.transform.Transformers;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class QueryDAOImpl implements QueryDAO {
 
     private  Session session;
